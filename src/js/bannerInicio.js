510acdc97b0386700c2f1cb0434c6c8e30b90a86
Marcos Sarges <dev.marcos.sarges@gmail.com>
2018-07-03 11:26:29 -0300

    poucas alteracoes

---
+++ src/js/bannerInicio.js
@@ -0,0 +1,5 @@
+var banner = document.querySelector('#banner');
+var tmJanela = window.innerHeight;
+
+banner.innerHTML = "Marcos Paulo";
+console.log(banner);
---
+++ src/js/sanfona.js
@@ -0,0 +1,25 @@
+$(function() {
+    $(".titulos").hover(
+        function() {
+            $(this).css("color", "#2e0");
+        },
+        function() {
+            $(this).css("color", "#fff");
+        }
+    )
+});
+
+
+
+var aberto = false;
+$(".titulos").click(function() {
+    // var cont = $(".conteudo").next();
+    var id = pegarElementoClicado();
+    if (aberto) {
+        $(id).slideUp();
+        aberto = !aberto;
+    } else {
+        $(id).slideDown();
+        aberto = !aberto;
+    }
+});
